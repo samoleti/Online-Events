bf4872262eb751c596c711884cb4c42a091ea5bc
spasova <tsvetina.spasova8@gmail.com>
2020-11-20 12:55:35 -0800

    Add the init of the project

---
+++ backend/src/main/java/com/fmi/online/events/OnlineEventsApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.online.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEventsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fmi/online/events/OnlineEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.online.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
